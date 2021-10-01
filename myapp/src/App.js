5e397bb9b5d376c526c449a384825a0b2989913b
Purab <purabdk@yahoo.co.in>
2021-10-01 19:02:09 +0530

    Lifecycle Methods added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -26,6 +26,13 @@ class App extends Component {
       ninjas:ninjas
     })
   }
+  componentDidMount() {
+    console.log('component mounted')    
+  }
+  componentDidUpdate(prevProps,prevState) {
+      console.log('component updated');
+      console.log(prevProps,prevState);
+  }
 render() {
   return (
     <div className="App">

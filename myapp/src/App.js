fd8e6bc152aa762d7ebba852ad8bea4d864f3f0a
Purab <purabdk@yahoo.co.in>
2021-10-01 17:30:40 +0530

    changes of list output

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,24 @@
+import React, { Component } from 'react';
 import Ninjas from "./Ninjas";
 
-
-
-function App() {
+class App extends Component {
+  state = {
+    ninjas : [
+      {name:"purab", age:30,belt:"black",id:1},
+      {name:"neha", age:33,belt:"red",id:2},
+      {name:"sanchi", age:32,belt:"green",id:3}
+    ]
+  }
+render() {
   return (
     <div className="App">
       <h1>My first react app</h1>
       <p>Welcome :)</p>    
-      <Ninjas name="Purab" age="30" belt="Black" />  
+      <Ninjas ninjas={this.state.ninjas} />  
     </div>
   );
 }
+}
+  
 
 export default App;

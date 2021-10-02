c3aca4f320ff2a5a20bf861c1313ea75b134d5da
Purab <purabdk@yahoo.co.in>
2021-10-02 11:34:19 +0530

    added reducers

--- todoapp/src/components/Post.js
+++ todoapp/src/components/Post.js
@@ -1,27 +1,22 @@
 import React, {Component} from "react";
-import axios from "axios";
+import {connect} from 'react-redux'
 
-class Post extends Component {
-    state = {
-        post:null
-    }
-    componentDidMount(){
-        console.log(this.props);
-        let id = this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
-            .then(res=>{
-                this.setState({
-                    post: res.data
-                })
-                console.log(res);                 
-            })   
-                
+class Post extends Component {    
+    handleClick = ()=> {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
     }
     render() {
-        const post = this.state.post ? (
+        console.log(this.props)
+        const post = this.props.post ? (
             <div className="post">
-                <h4>{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4>{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>            
         ) : (
             <div className="center">No Post</div>
@@ -34,4 +29,18 @@ class Post extends Component {
     }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    return {
+        post: state.posts.find( (post) => {
+            return post.id === id
+        })
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => {dispatch({type:'DELETE_POST',id: id})}
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Post);

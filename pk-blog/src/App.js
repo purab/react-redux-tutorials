81a28836a12bb5badbb638eb88a1402f29c9b009
Purab <purabdk@yahoo.co.in>
2021-10-02 16:38:14 +0530

    adding router url

--- pk-blog/src/App.js
+++ pk-blog/src/App.js
@@ -1,14 +1,26 @@
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Create from './components/Create';
 
 function App() {  
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />        
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path='/' >
+                <Home />
+              </Route>
+              <Route path='/create' >
+                <Create />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+    </Router>
+    
   );
 }
 

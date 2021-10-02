eb4fa3170a46de62e8dcd7d8c638fd65b0d7db67
Purab <purabdk@yahoo.co.in>
2021-10-02 16:08:05 +0530

    added error handling

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -3,22 +3,35 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending,setIsPending] = useState(true)
+    const [error,setError] = useState(null)
+
     useEffect(()=>{
         console.log('use effect ran ')
         fetch('http://localhost:8000/blogs')
             .then(res =>{
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
                 return res.json()
             })
             .then(data => {
                 console.log(data)
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch((err) => {
+                setIsPending(false)
+                setError(err.message)
             })
         
     },[])
     return ( 
         <div className="home">
-           {blogs && <BlogList blogs={blogs} title={'All Blogs!'} /> }
-           
+            {error && <div> {error}</div>}
+            {isPending && <div>loading...</div>}
+           {blogs && <BlogList blogs={blogs} title={'All Blogs!'} /> }           
         </div>
      );
 }

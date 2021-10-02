83f723540fa54e502e13ee5033348c7fda64d561
Purab <purabdk@yahoo.co.in>
2021-10-02 17:38:55 +0530

    added loading for add blog

--- pk-blog/src/components/Create.js
+++ pk-blog/src/components/Create.js
@@ -4,11 +4,29 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const blog ={title, body,author}
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs',{
+        method:'POST',
+        headers:{"Content-type":"application/json"},
+        body: JSON.stringify(blog)
+    }).then(()=>{
+        console.log('new blog added');
+        setIsPending(false)
+    })
+        
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text" 
@@ -30,7 +48,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog....</button>}
       </form>
     </div>
   );

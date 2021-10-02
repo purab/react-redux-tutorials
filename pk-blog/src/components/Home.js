7d1b01c8eafae972f6d001d7f45ec73357b817d0
Purab <purabdk@yahoo.co.in>
2021-10-02 13:46:40 +0530

    using custom functions

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello ninjas'+e)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }

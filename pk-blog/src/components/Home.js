ada26b22a77e8b96bfb4ca00db1bfdb4b9257021
Purab <purabdk@yahoo.co.in>
2021-10-02 14:09:56 +0530

    adding blog list

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -1,11 +1,20 @@
+import {useState} from 'react';
+
 const Home = () => {
-    const handleClick = (e) => {
-        console.log('hello ninjas'+e)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+
         </div>
      );
 }

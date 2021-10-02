e096caa92471c61aab2c3a03fae340f5aaf51365
Purab <purabdk@yahoo.co.in>
2021-10-02 14:24:41 +0530

    updated props

--- pk-blog/src/components/BlogList.js
+++ pk-blog/src/components/BlogList.js
@@ -1,6 +1,5 @@
-const BlogList = (props) => {
-    const blogs = props.blogs
-    const title = props.title;
+const BlogList = ({blogs, title}) => {
+    
     return (
         <div className="blog-list">
             <h2>{title}</h2>

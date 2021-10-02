3b3ec9dd7def8a4167b91964ab3d7caea74b1107
Purab <purabdk@yahoo.co.in>
2021-10-02 15:11:36 +0530

    adding db json

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -2,23 +2,22 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-    
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog =>blog.id !==id)
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null)
     useEffect(()=>{
         console.log('use effect ran ')
+        fetch('http://localhost:8000/blogs')
+            .then(res =>{
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setBlogs(data)
+            })
         
     },[])
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title={'All Blogs!'} handleDelete={handleDelete}/>
+           {blogs && <BlogList blogs={blogs} title={'All Blogs!'} /> }
            
         </div>
      );

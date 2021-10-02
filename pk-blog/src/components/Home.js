b09120a1b6294b677d80ad7c641f6d6f664e3883
Purab <purabdk@yahoo.co.in>
2021-10-02 14:57:10 +0530

    adding json

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -7,13 +7,19 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog =>blog.id !==id)
         setBlogs(newBlogs);
     }
+    useEffect(()=>{
+        console.log('use effect ran ')
+        
+    },[])
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title={'All Blogs!'} handleDelete={handleDelete}/>
+           
         </div>
      );
 }

54ec067f7a23b0c3a8bc0327d8a80593c9727a3f
Purab <purabdk@yahoo.co.in>
2021-10-02 14:44:25 +0530

    delete blogs

--- pk-blog/src/components/Home.js
+++ pk-blog/src/components/Home.js
@@ -7,9 +7,13 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog =>blog.id !==id)
+        setBlogs(newBlogs);
+    }
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title={'All Blogs!'} />
+           <BlogList blogs={blogs} title={'All Blogs!'} handleDelete={handleDelete}/>
         </div>
      );
 }
